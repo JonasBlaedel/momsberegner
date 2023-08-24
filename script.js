67044eb86fc118c466c60d6ae6d8807768ee8d92
JonasBlaedel <119848473+JonasBlaedel@users.noreply.github.com>
2023-08-24 11:42:10 +0200

    final

---
+++ script.js
@@ -0,0 +1,16 @@
+momsberegner(24, 47);
+
+function momsberegner(beloeb, moms) {
+  console.log(beloeb * (1 + moms / 100));
+}
+
+// document.querySelector("button").addEventListener("click", tax);
+
+// function tax(amount, tax = 1.25) {
+//   amount = document.getElementById("amount").value;
+//   tax = document.getElementById("tax").value;
+
+//   let samlet = amount * (1 + tax / 100);
+
+//   document.querySelector("h2").textContent = `Beløbet er ${samlet}`;
+// }
